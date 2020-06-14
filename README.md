e209202962ff0e3f90e1db00ebe4826ef381de0d
hogus <wugang@taopaitang.com>
2020-06-14 23:14:37 +0800

    support postgres

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-<h1 align="center"> laravel-schema-mysql-extend </h1>
+<h1 align="center"> laravel-schema-extend </h1>
 
-<p align="center"> laravel schema mysql extend.</p>
+<p align="center"> laravel schema table comments extend.</p>
 
 
 ## Installing
@@ -11,11 +11,22 @@ $ composer require hogus/laravel-schema-mysql-extend -vvv
 
 ## Usage
 
+### 1.0
 ```php
 Schema::table('example', function ($table) {
    $table->id();
     //......
    $table->comment('comment'); // Add Mysql Table Comments
+ });
+```
+
+### 2.0
+```php
+# support mysql and postgres
+Schema::table('example', function ($table) {
+   $table->id();
+    //......
+   $table->tableComment('comment'); // Add Table Comments
  });
 ```
 
--- src/GrammarServiceProvider.php
+++ src/GrammarServiceProvider.php
@@ -5,30 +5,33 @@ namespace Hogus\LaravelSchemaMysqlExtend;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Grammars\Grammar;
 use Illuminate\Database\Schema\Grammars\MySqlGrammar;
+use Illuminate\Database\Schema\Grammars\PostgresGrammar;
 use Illuminate\Support\ServiceProvider;
 
-class MySqlGrammarServiceProvider extends ServiceProvider
+class GrammarServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        Blueprint::macro('comment', function ($index) {
-            return $this->addCommand('comment', compact('index'));
+        Blueprint::macro('tableComment', function ($index) {
+            return $this->addCommand('tableComment', compact('index'));
         });
 
         /**
-         * @example $table->comment('comment')
+         * @example $table->tableComment('comment')
          *
          * Schema::table('example', function ($table) {
          *      $table->id();
          *      ......
-         *      $table->comment('comment'); // Add Mysql Table Comments
+         *      $table->tableComment('comment'); // Add Mysql Table Comments
          * })
          */
         Grammar::macro('compileComment', function ($blueprint, $command) {
             if ($blueprint instanceof MySqlGrammar) {
                 return "alter table {$this->wrapTable($blueprint)} comment = '{$command->index}' ";
+            } elseif ($blueprint instanceof PostgresGrammar) {
+                return "comment on table {$this->wrapTable($blueprint)} is '{$command->index}' ";
             } else {
-                throw new \Exception('Only supports mysql driver');
+                throw new \Exception('Only supports mysql and postgres driver');
             }
         });
     }
