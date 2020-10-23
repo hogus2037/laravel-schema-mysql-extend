fe3b3e24ab75f3355e0986db9f4e0a0dbcdb35eb
hogus <hogus2037@gmail.com>
2020-10-23 10:15:28 +0800

    macro class method

--- src/GrammarServiceProvider.php
+++ src/GrammarServiceProvider.php
@@ -2,33 +2,33 @@
 
 namespace Hogus\LaravelSchemaMysqlExtend;
 
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\PostgresConnection;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Grammars\Grammar;
-use Illuminate\Database\Schema\Grammars\MySqlGrammar;
-use Illuminate\Database\Schema\Grammars\PostgresGrammar;
 use Illuminate\Support\ServiceProvider;
 
 class GrammarServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        Blueprint::macro('tableComment', function ($index) {
-            return $this->addCommand('tableComment', compact('index'));
+        Blueprint::macro('comment', function ($index) {
+            return $this->addCommand('comment', compact('index'));
         });
 
         /**
-         * @example $table->tableComment('comment')
+         * @example $table->comment('comment')
          *
          * Schema::table('example', function ($table) {
          *      $table->id();
          *      ......
-         *      $table->tableComment('comment'); // Add Mysql Table Comments
+         *      $table->comment('comment'); // Add Mysql Table Comments
          * })
          */
-        Grammar::macro('compileComment', function ($blueprint, $command) {
-            if ($blueprint instanceof MySqlGrammar) {
+        Grammar::macro('compileComment', function ($blueprint, $command, $connection) {
+            if ($connection instanceof MySqlConnection) {
                 return "alter table {$this->wrapTable($blueprint)} comment = '{$command->index}' ";
-            } elseif ($blueprint instanceof PostgresGrammar) {
+            } elseif ($connection instanceof PostgresConnection) {
                 return "comment on table {$this->wrapTable($blueprint)} is '{$command->index}' ";
             } else {
                 throw new \Exception('Only supports mysql and postgres driver');
